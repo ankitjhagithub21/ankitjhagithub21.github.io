94e5eba6ff3d583de8b8f5dad392de181b699889
ankitjhagithub21 <ajha45075@gmail.com>
2025-06-26 01:11:16 +0530

    minor changes

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const typed = new Typed('#typed', {
   strings: ['Full Stack Developer', "Open Source Contributor", 'Freelancer'],
   typeSpeed: 60,
-  loop:true
+  loop:true,
+  cursorChar: ''
 });
