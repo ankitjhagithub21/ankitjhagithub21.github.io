f8277dabec0efc060d68c9887f5d396ec0e72fef
ankitjhagithub21 <ajha45075@gmail.com>
2025-06-27 00:34:38 +0530

    changes background

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-const typed = new Typed('#typed', {
-  strings: ['Full Stack Developer', "Open Source Contributor", 'Freelancer'],
-  typeSpeed: 60,
-  loop:true,
-  cursorChar: ''
+
+
+function startLocoMotive(){
+  const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
 });
+}
+
+startLocoMotive()
+
+function startTyping() {
+
+  const typed = new Typed('#typed', {
+    strings: ['Full Stack Developer', "Open Source Contributor", 'Freelancer'],
+    typeSpeed: 60,
+    loop: true,
+    cursorChar: ''
+  });
+
+
+}
+
+startTyping()
